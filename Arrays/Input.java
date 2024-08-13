7f11d530393ba26e341bf88d7a7bf36bef95b1dc
Ankit Dhakad <ankitdhakad359@gmail.com>
2024-08-13 20:46:27 +0530

    Input.java file is added

---
+++ Arrays/Input.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Input {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        arr[0] = 32;
+        arr[1] = 46;
+        arr[2] = 63;
+        arr[3] = 325;
+        arr[4] = 3161;
+
+        System.out.println(arr[4]);
+    }
+}
